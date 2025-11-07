2b6f3622d3f5b08e38cb9fa2ad28228ddf798d97
cbhayes77 <christopher.brandon.hayes@gmail.com>
2025-11-06 19:57:57 -0600

    Added TechStack section with icons.

---
+++ src/data/techstack.js
@@ -0,0 +1,37 @@
+export const techstack = [
+  {
+    id: "html5",
+    label: "HTML5",
+    href: "",
+    iconId: "html5",
+    color: "#E34F26",
+  },
+  {
+    id: "css3",
+    label: "CSS3",
+    href: "",
+    iconId: "css3",
+    color: "#0099e5",
+  },
+  {
+    id: "javascript",
+    label: "Javascript",
+    href: "",
+    iconId: "javascript",
+    color: "#f7df1e",
+  },
+  {
+    id: "react",
+    label: "React",
+    href: "",
+    iconId: "react",
+    color: "#00d8ff",
+  },
+  {
+    id: "figma",
+    label: "Figma",
+    href: "",
+    iconId: "figma",
+    color: "#FD3F39",
+  },
+];
